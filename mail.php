6634e58f03427ce6973db815d9c8b5ea020bdb4d
vaishwadenitin@gmail.com <vaishwadenitin@gmail.com>
2016-12-02 14:26:12 +0530

    php script testing

--- mail.php
+++ mail.php
@@ -4,15 +4,12 @@
 <?php
 $to="nitinvaishwade@gmail.com";
 $subject="Qtpi Contact people";
-$email=$_REQUEST['email'];
-$message=$_REQUEST['message'];
+$email=$_POST['email'];
+$message=$_POST['message'];
 $header="From:$email";
 mail($to,$subject,$message,$header);
 echo"your message has been send";    
-//if($send)
-//{print('<a href="www.qtpi.in">Thank you for contecting us</a>');}
-//else
-//{print"We encounted an error sending your mail";}
+
 ?>
 
 </body>
