32f3d97aa3063ce8002726a556f50878c6bc3d7e
vaishwadenitin@gmail.com <vaishwadenitin@gmail.com>
2016-12-02 14:08:58 +0530

    change script setting

--- mail.php
+++ mail.php
@@ -7,11 +7,12 @@ $subject="Qtpi Contact people";
 $email=$_REQUEST['email'];
 $message=$_REQUEST['message'];
 $header="From:$email";
-$send=mail($to,$subject,$message,$header);
-if($send)
-{print('<a href="www.qtpi.in">Thank you for contecting us</a>');}
-else
-{print"We encounted an error sending your mail";}
+mail($to,$subject,$message,$header);
+echo"your message has been send";    
+//if($send)
+//{print('<a href="www.qtpi.in">Thank you for contecting us</a>');}
+//else
+//{print"We encounted an error sending your mail";}
 ?>
 
 </body>
