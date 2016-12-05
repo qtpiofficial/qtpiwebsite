647c23f621a5754ff2271aef7d3ee2f7215774e0
vaishwadenitin@gmail.com <vaishwadenitin@gmail.com>
2016-12-05 12:14:39 +0530

    change script for sending mail

--- mail.php
+++ mail.php
@@ -1,5 +1,4 @@
-<html>
-<body>
+
 
 <?php
 $to="nitin@qtpi.in";
@@ -7,10 +6,15 @@ $subject="Qtpi Contact people";
 $email=$_POST['email'];
 $message=$_POST['message'];
 $header="From:$email";
-mail($to,$subject,$message,$header);
-echo"your message has been send";
-
+// mail($to,$subject,$message,$header);
+// echo"your message has been send";
+if (mail($to,$subject,$message,$header))
+   {
+       print "Your message is sent.";
+   }
+   else
+   {
+       print "Your message is not sent.
+       <br>Please go <a href=\"javascript:history.back();\">back</a> and send again.";
+   }
 ?>
-
-</body>
-</html>
