7713cb408bef8147a3075aacb46a1b7644705d6a
vaishwadenitin@gmail.com <vaishwadenitin@gmail.com>
2016-12-05 16:45:30 +0530

    add php mailer script

---
+++ example.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'class.phpmailer.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'in-v3.mailjet.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '521bdd8fda2c388967902217d1738666
+';                 // SMTP username
+$mail->Password = '24ed9a7faff327b2de77b1dc6ce0bbc4';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('nitin@qtpi.in', 'Mailer');
+$mail->addAddress('nitinvaishwade@gmail.com', 'nitin vaishwade');     // Add a recipient
+$mail->addAddress('nitin@qtpi.in');               // Name is optional
+$mail->addReplyTo('nitin@qtpi.in', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
